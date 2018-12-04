3387dc6da439e0111c5a68aa99e20b11d87e1694
root <root@HPS6SSH1.hpsmgmt.local>
2018-12-04 11:33:47 -0500

    edits on loginaudit.sh

--- loginaudit.sh
+++ loginaudit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ######################################################################################################################
-#Location: Currently running on OTSUATERP64.hps-ots.local and OTSPRDDB.hps-ots.local
+#Location: Currently running on HOSTS OMITTED
 #Filename: /root/loginaudit.sh
 #Behavior: On the first day of the month, it will get the previous month's failed and successful logins and sudo attempts.
 #Notes: ignoring oraots/oraauxr. they are service accounts that spam the logs. ignore cimserver. acknowledged and spam. secure.* works fine but we need to sort the dates.
@@ -25,7 +25,8 @@ grep -i fail /var/log/secure | grep -i $LASTMONTH | grep -v oraots | grep -v ora
 printf "\n===========================Sudoers Activity======================\n" >> /root/loginaudit$LASTMONTH.txt
 
 grep -i sudo /var/log/secure.* >> /root/loginaudit$LASTMONTH.txt
-mail -s "OTSPRDDB $LASTMONTH Login Report" kristopher.kahn@highpointsolutions.com anand.valluri-cw@otsuka-us.com < /root/loginaudit$LASTMONTH.txt
+mail -s "HOSTNAME $LASTMONTH Login Report" kristopher.kahn@highpointsolutions.com < /root/loginaudit$LASTMONTH.txt
 mv /root/loginaudit$LASTMONTH.txt /root/logins
 
-#30 15 * * * /opt/VRTSralus/bin/VRTSralus.init restart >/dev/null 2>&1
+#crontab. runs first of the month at 5 AM
+#0 5 1 * * /root/loginaudit.sh 
